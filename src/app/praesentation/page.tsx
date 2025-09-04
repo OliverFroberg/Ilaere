16e334da971943a78acb5e777b81bdaf71e1d952
amberbanana5078 <alexboyekristiansen@gmail.com>
2025-09-04 10:06:54 +0200

    ijpda

--- src/app/praesentation/page.tsx
+++ src/app/praesentation/page.tsx
@@ -1,13 +1,11 @@
 export default function Page() {
 	return (
 		<>
-			<h1>Præsentation</h1>
-			<div className="contianer m-auto bg-amber-700 w-500 h-500">
-				<div className="m-auto grid grid-cols-12" >
-				<article className=" col-span-6" ></article>
-				<section className=" col-span-6"></section>
-				
-				
+			
+			<div className="flex flex-col w-full h-auto gap-5">
+				<video  className="w-4/5 h-auto self-center" src="/campus.mp4"></video>
+				<div className="w-4/5 bg">
+
 				</div>
 			</div>
 		</>
